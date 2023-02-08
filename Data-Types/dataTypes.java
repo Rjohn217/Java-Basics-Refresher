8ab32aaec6de5d60718249d0db5e990c08a71ff7
Richard Austin Johnson <86606835+Rjohn217@users.noreply.github.com>
2023-02-08 18:44:53 +0000

    "dataTypes file added."

---
+++ Data-Types/dataTypes.java
@@ -0,0 +1,27 @@
+public class dataTypes {
+
+    public static void main(String[] args) {
+    
+            // Here you will find the various data types used in Java
+    
+            byte b = 1;
+    
+            short s = 10;
+    
+            int i = 1000;
+    
+            long l = 123456789;
+    
+            double d = 2.50;
+    
+            boolean areWeLearning = true;
+    
+    
+            String myString = "This is a string!";
+    
+            // We can print these out to the command line using a 'print line' statement:
+    
+            System.out.println(b + " " + s + " " + i + " " + l + " " + d + " " + areWeLearning  + " " + myString);
+    
+    }
+    }
